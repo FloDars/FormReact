94312e15b0e82870fdd340dc919e53112ac310c1
fmia454 <florian.miaut@darva.com>
2025-09-29 12:05:54 +0200

    exo 5

--- 05_arrayOperations.js
+++ 05_arrayOperations.js
@@ -10,7 +10,7 @@
  * 
   */
 
-const multiplyByTwo = [1,2].map(i => i * 2);
+const multiplyByTwo = (a) => a.map(i => i * 2);
 
 
 /**
@@ -23,7 +23,7 @@ const multiplyByTwo = [1,2].map(i => i * 2);
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const filterNameStartByA = ["Arn", "Damien"].filter(i => i.startsWith("A"));
+const filterNameStartByA = (a) => a.filter(i => i.startsWith("A"));
 
 /**
  * Utiliser la fonction .reduce sur le tableau passé en paramètre
@@ -35,7 +35,7 @@ const filterNameStartByA = ["Arn", "Damien"].filter(i => i.startsWith("A"));
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const sum = [0, 1, 2, 3].reduce((a, b) => a + b, 0);
+const sum = (a) => a.reduce((a, b) => a + b, 0);
 
 /**
  * Utiliser la fonction .find sur le tableau passé en paramètre
